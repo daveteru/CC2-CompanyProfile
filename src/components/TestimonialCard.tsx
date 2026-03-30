3794c09918e9daf8f2cd62d7d4ed6e44f400e0aa
Teru <218044131+daveteru@users.noreply.github.com>
2026-03-30 11:20:39 +0700

    style: simplified UI

--- src/components/TestimonialCard.tsx
+++ src/components/TestimonialCard.tsx
@@ -6,9 +6,8 @@ interface TestimonialCardProps {
 
 export default function TestimonialCard({ name, location, review }: TestimonialCardProps) {
   return (
-    <div className="bg-[#B0E4E8] text-center md:text-left w-fit h-full rounded-3xl justify-between flex flex-col p-10 gap-5 drop-shadow-[0px_8px_0px_rgba(25,118,210,1)]">
-      <div className="flex flex-col md:flex-row gap-5 items-center">
-        <div className="w-20 h-20 bg-[#1976D2] rounded-full"></div>
+    <div className="bg-[#B0E4E8] text-center md:text-left w-fit min-h-50 rounded-3xl justify-between flex flex-col p-10 gap-5 drop-shadow-[0px_8px_0px_rgba(25,118,210,1)]">
+      <div className="flex flex-col md:flex-row gap-5 items-center ">
         <div className="flex flex-col">
           <h2 className="text-xl">{name}</h2>
           <p>{location}</p>
